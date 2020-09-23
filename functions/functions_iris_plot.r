19d1ba5c6f34de40a4907b7432af4a662177c6f5
Chris <johnvickers72@yahoo.com>
2020-09-22 20:35:57 -0700

    common basic functions

---
+++ functions/functions_iris_plot.r
@@ -0,0 +1,5 @@
+pairs	(
+	iris[1:4], 
+	main="Iris Data (red=setosa,green=versicolor,blue=virginica)", 
+	cex.main = .9, pch=21, bg=c("red", "green3", "blue") [unclass(iris$Species)], cex.labels = .9
+    )
