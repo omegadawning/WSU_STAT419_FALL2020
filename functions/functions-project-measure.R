55df0abc6030361667a225369c5284e3974f8ae6
omegadawning <johnvickers72@yahoo.com>
2020-11-10 22:34:16 -0800

    Add files via upload

--- functions/functions-project-measure.R
+++ functions/functions-project-measure.R
@@ -62,7 +62,7 @@ sep_Female = function(x)
   return(measure_female);
   }
 
-subset_armMeasurements = function(x)
+arm_measurementsSubset = function(x)
   {
   # pull arm and height data
   x.1 <- x[,c(4,7,8,9,10,11,12,13,14,17)];
